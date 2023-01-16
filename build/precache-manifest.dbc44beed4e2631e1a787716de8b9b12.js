f115671e05d8bd625c82b15a0ad92557845a3d71
Raka De <rakaday@gmail.com>
2023-01-15 19:41:01 -0500

    change made to website, formatting and sorting table

---
+++ build/precache-manifest.dbc44beed4e2631e1a787716de8b9b12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20226514fcc672180d6852bd1486d98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2881e5d79253f3390004",
+    "url": "/static/css/main.66a05054.chunk.css"
+  },
+  {
+    "revision": "643534835f3eb5d445a9",
+    "url": "/static/js/2.8c1afa42.chunk.js"
+  },
+  {
+    "revision": "2881e5d79253f3390004",
+    "url": "/static/js/main.6f144352.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
